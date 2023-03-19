49247c5e25dc62de4220c9e9685a6c177c972f3a
ZIAR Parvaiz <ziarparvaiz@gmail.com>
2023-03-19 22:33:59 +0300

    feat: card component is added

---
+++ components/ui/Card.js
@@ -0,0 +1,19 @@
+import classNames from "classnames";
+
+function Card({ children, ...props }) {
+  return (
+    <div
+      {...props}
+      className={classNames(
+        {
+          "p-[20px] rounded-[15px] bg-transparent border-[1px] border-gray-400": true,
+        },
+        props.className
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Card;
