0f856266f0bd79bce4c3fdccf281e9bb633408e3
ZIAR Parvaiz <ziarparvaiz@gmail.com>
2023-03-19 22:32:30 +0300

    feat: header and footer component is added

---
+++ components/global/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <>
+      <span className="text-[15px] text-white-300 font-light text-center py-4">
+        © Copyright 2023 DevFinder LLC. All rights reserved.
+      </span>
+    </>
+  );
+}
+
+export default Footer;
---
+++ components/global/Header.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { SiVercel } from "react-icons/si";
+import { AiFillGithub } from "react-icons/ai";
+import Button from "../ui/Button";
+
+function Header() {
+  return (
+    <div className="flex justify-between items-center h-12 my-4">
+      <Link
+        className="flex items-center gap-3 text-[20px] font-semibold"
+        href="/"
+      >
+        <SiVercel />
+        <span>DevFinder</span>
+      </Link>
+      <div className="flex items-center gap-4">
+        <Button
+          className="text-gray-500 hover:text-white transition duration-300 ease-in-out"
+          href="/docs"
+        >
+          Docs
+        </Button>
+        <Button
+          border="border"
+          className="border-dark-300 text-gray-500 hover:text-white hover:border-white transition duration-300 ease-in-out"
+          href="/github"
+        >
+          <AiFillGithub />
+          <span>GitHub</span>
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
